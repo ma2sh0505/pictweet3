b6fd13a9ec18debb867231f0324ca9d0dfa88050
ma2sh0505 <ma284sh05@gmail.com>
2021-02-13 12:17:50 +0900

    マイページ実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show]
-  
+
   def index
     @tweets = Tweet.all
   end
@@ -32,10 +32,16 @@ class TweetsController < ApplicationController
 
 private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
